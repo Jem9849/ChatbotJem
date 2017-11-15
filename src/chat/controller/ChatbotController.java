6492ba5222b9620b7bf6f2ac2e0b0915be0d0fc9
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2017-11-15 12:26:52 -0700

    Added in the ChatFrame into my controller giving it a variable into appFrame. Also I commented something out.

--- src/chat/controller/ChatbotController.java
+++ src/chat/controller/ChatbotController.java
@@ -2,28 +2,32 @@ package chat.controller;
 
 import chat.model.Chatbot;
 import chat.view.PopupDisplay;
+import chat.view.ChatFrame;
 
 public class ChatbotController
 {
 	 private Chatbot chatbot;
 	 private PopupDisplay display;
-	 //private ChatFrame appFrame;
+	 private ChatFrame appFrame;
 	 
 	 public ChatbotController() // Makes an instance of Chatbot and PopupDisplay.
 	 {
 		 chatbot = new Chatbot("MURRRR");
 		 display = new PopupDisplay();
+		 appFrame = new ChatFrame(this);
 	 }
 	 
 	public void start() // Makes a string called response which displays takes to gather a response from a user. It then checks the response to see whether it says quit, or if it is the correct length.
 	{
 		String response = display.collectResponse("What do you want to talk about?");
 		
-		while (chatbot.lengthChecker(response) && !chatbot.quitChecker(response)) 
+	/*	while (chatbot.lengthChecker(response) && !chatbot.quitChecker(response)) 
 		{
 			response = popupChat(response); 
 			response = display.collectResponse(response);
 		}
+	*/
+		
 	}
 	
 	public String interactWithChatbot(String input)
