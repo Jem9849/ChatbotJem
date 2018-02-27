b73dbf08593530070241429a86cfbb1919d3f061
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-02-27 11:52:42 -0700

    Added a common word text file and added a java class that reads text files.

---
+++ src/chat/controller/IOController.java
@@ -0,0 +1,40 @@
+package chat.controller;
+
+import java.io.*;
+import java.util.*;
+
+public class IOController 
+{
+	public static void saveToFile(ChatbotController app, String textToSave, String path)
+	{
+		
+	}
+	
+	public static String loadFromFile(ChatbotController app, String filename)
+	{
+		String results = "";
+		
+		try
+		{
+			// Opens at root level of project.
+			
+			File openFile = new File(filename);
+			Scanner fileScanner = new Scanner(openFile);
+			
+			String currentLine = fileScanner.nextLine();
+			while (fileScanner.hasNextLine())
+			{
+				results += currentLine + "\n";
+				currentLine = fileScanner.nextLine();
+			}
+			
+			results += currentLine + "\n";
+			fileScanner.close();
+		}
+		catch (IOException error)
+		{
+			app.handleErrors(error);
+		}
+		return results;
+	}
+}
