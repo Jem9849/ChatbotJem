6dd599bfae3b6bb5a3733e3fec238564807fa38b
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-03-01 12:48:41 -0700

    Added code to createIgnoredWordArray and fixed a type mismatch.

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -57,6 +57,7 @@ public class CTECTwitter
 		collectTweets(username);
 		turnStatusesToWords();
 		totalWordCount = tweetedWords.size();
+		String [] boring = createIgnoredWordArray();
 		
 		return mostCommon;
 	}
@@ -120,8 +121,34 @@ public class CTECTwitter
 		return scrubbedString;
 	}
 	
-//	private String createIgnoredWordArray()
-//	{
-//		
-//	}
+	private String [] createIgnoredWordArray()
+	{
+		String [] boringWords;
+		String fileText = IOController.loadFromFile(appController, "commonWords.txt");
+		int wordCount = 0;
+		
+		Scanner wordScanner = new Scanner(fileText);
+		
+		while (wordScanner.hasNextLine())
+		{
+			wordScanner.nextLine();
+			wordCount++;
+		}
+		
+		boringWords = new String [wordCount];
+		wordScanner.close();
+		
+		// Alternative file loading method.
+		// Uses the inputstream class.
+		// Notice the lack of try/catch.
+		
+		wordScanner = new Scanner(this.getClass().getResourceAsStream("commonWords.txt")); // "data/commonWords.txt"
+		for (int i = 0; i < boringWords.length; i ++)
+		{
+			boringWords[i] = wordScanner.nextLine();
+		}
+		
+		wordScanner.close();
+		return boringWords;
+	}
 }
